df972e939fa900086661bca7ac58faaa9a36c2cd
nguyen-thinh109 <thinh.nguyenduc1@vdigital.vn>
2023-05-27 11:51:08 +0700

    minimize function 4 chatbot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,32 @@ export class AppComponent {
   title = 'TEST CHAT BOT';
   isChatOpened: boolean = false;
 
-  showChatFrame():void {
+  constructor() {
+    window.addEventListener('message', (e) => {
+      let dataTransport = JSON.parse(e.data) || {};
+
+      if (dataTransport?.topic == "BUTTON_CLICK") {
+        switch (dataTransport?.buttonAction) {
+          case "MINIMIZE":
+            console.log(dataTransport);
+            this.isChatOpened = !this.isChatOpened;
+
+            break;
+
+          case "XXX":
+            //do XXX
+            break;
+
+          case "YYY":
+            //do YYY
+            break;
+
+        }
+      }
+    });
+  }
+
+  showChatFrame(): void {
     this.isChatOpened = !this.isChatOpened;
   }
 }
