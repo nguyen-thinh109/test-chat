2b64d71f122b40ed9098ea2c395558f0de810237
nguyen-thinh109 <thinh.nguyenduc1@vdigital.vn>
2023-05-27 19:22:36 +0700

    scale iframe by screen size

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,32 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'TEST CHAT BOT';
   isChatOpened: boolean = false;
+  screenWidth!: number;
+  screenHeight!: number;
+  iframeWidth: number = 360;
+  iframeHeight: number = 640;
+  isMobileMode: boolean = false;
 
   constructor() {
+    this.screenHeight = window.innerHeight;
+    this.screenWidth = window.innerWidth;
+
+    //ratio 16:9
+    if (this.screenWidth <= 575) {
+      this.iframeWidth = this.screenWidth;
+      this.iframeHeight = this.screenHeight;
+      this.isMobileMode = true;
+    } else if (this.screenWidth > 575 && this.screenWidth <= 767) {
+      this.iframeWidth = this.screenWidth * 2/3;
+      this.iframeHeight = this.screenWidth * 32/27;
+      this.isMobileMode = false;
+    } else {
+      this.isMobileMode = false;
+      this.iframeHeight = this.screenHeight * 2/3;
+      this.iframeWidth = this.screenHeight * 3/8;
+      console.log(this.screenHeight, this.screenWidth, this.iframeHeight, this.iframeWidth)
+    }
+
     window.addEventListener('message', (e) => {
       let dataTransport = JSON.parse(e.data) || {};
 
